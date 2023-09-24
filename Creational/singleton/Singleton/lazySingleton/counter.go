7794f0acc6542caac9e6b8a5486701c7bd8f262a
xince <3190101570@zju.edu.cn>
2023-09-24 16:08:39 +0800

    Add Singleton Code

--- Creational/singleton/Singleton/lazySingleton/counter.go
+++ Creational/singleton/Singleton/lazySingleton/counter.go
@@ -2,6 +2,12 @@ package singleton
 
 import "sync"
 
+// Singleton 是单例模式接口，导出的
+// 通过该接口可以避免 GetInstance 返回一个包私有类型的指针
+type Singleton interface {
+	AddOne() int
+}
+
 type singleton struct {
 	count int
 }
@@ -11,9 +17,9 @@ var (
 	instance *singleton
 )
 
-func GetInstance() *singleton {
+func GetInstance() Singleton {
 	once.Do(func() {
-		instance = new(singleton)
+		instance = &singleton{}
 	})
 
 	return instance
